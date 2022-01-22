200e88b9067b4cc6e2337a659cc13220a2533b3b
SovietSupporter(Desktop) <hibfumcuvlf@gmail.com>
2022-01-22 17:10:03 +0000

    Mostly finished taking the input and turning it into a sorted class.

--- Rando Map Recreator/Form1.cs
+++ Rando Map Recreator/Form1.cs
@@ -14,6 +14,7 @@ namespace Rando_Map_Recreator
     public partial class Form1 : Form
     {
         List<string> CheckedTransitions = new List<string>();
+        List<Transition_link> Transitions = new List<Transition_link>();
 
         public Form1()
         {
@@ -24,7 +25,7 @@ namespace Rando_Map_Recreator
         private void button1_Click(object sender, EventArgs e)
         {
             GetText();
-            //SortInput();
+            SortInput();
         }
 
         void GetText()
@@ -32,45 +33,62 @@ namespace Rando_Map_Recreator
             string FileName = @"C:\Users\User\AppData\LocalLow\Team Cherry\Hollow Knight\Randomizer 4\Recent\HelperLog.txt";
             try
             {
-                using (StreamReader reader = new StreamReader(FileName))
+                using (StreamReader reader = new StreamReader(FileName)) //opens the file
                 {
                     string line;
                     bool CorrectLine = false;
-                    while ((line = reader.ReadLine()) != null)
+                    while ((line = reader.ReadLine()) != null) //while there is a line to read
                     {
-                        if (line.StartsWith("CHECKED TRANSITIONS"))
+                        if (line.StartsWith("CHECKED TRANSITIONS"))  //if you find 'CHECKED TRANSITIONS' start saving lines
                         {
                             CorrectLine = true;
                         }
-                        if (line.StartsWith("RESPAWNING ITEMS"))
+                        if (line.StartsWith("RESPAWNING ITEMS")) //stop recording lines once you get to the next section
                         {
                             CorrectLine = false;
                         }
 
                         if (CorrectLine == true)
                         {
-                            CheckedTransitions.Add(line);
+                            CheckedTransitions.Add(line); //record the lines that are are meant to be
                         }
                     }
                 }
             }
             catch(Exception exp)
             {
-                textBox1.AppendText(exp.Message);
+                textBox1.AppendText(exp.Message); //give an error if recording goes wrong
             }
         }
 
         void SortInput()
         {
-            //in a for loop
-            //take the input
-            //check for shared characters
-            //separate out the right part for the room
+            char[] CheckingChars = {']', '[', ' '};
+            char[] CheckingNumb = { '0', '1', '2', '3', '4', '5' };
+            bool Repeat = false;
+            for (int i = 1; i < (CheckedTransitions.Count); i++)
+            {
+                string[] words = CheckedTransitions[i].Split(CheckingChars); //splits the raw line into devisions
+                Repeat = false;
+
+                for (int b = 0; b < Transitions.Count; b++) //checks if this transition is already recorded
+                {
+                    if ((Transitions[b].EntryRoom == words[8] && Transitions[b].ExitRoom == words[2]) || (Transitions[b].EntryRoom == words[2] && Transitions[b].ExitRoom == words[8]))
+                    {
+                        Repeat = true;
+                    }
+                }
+                if (Repeat == false) //if the transition is new
+                {
+                    Transitions[i-1].EntryRoom = words[2]; //and saves them in the class list (Rooms)
+                    Transitions[i-1].ExitRoom = words[8];
+
+                    string[] DirecNumb = CheckedTransitions[1].Split(CheckingNumb); //splits directions so that the number can be found
+                    textBox1.AppendText($"<{DirecNumb}>");
+                }
+            }
+
             //get the direction and and asign it a compas and number
-            //repeat once for the exit
-            //check if this transition is already in the class
-            //in new input into class
-            //repeat for all of the transitions
         }
     }
 }
